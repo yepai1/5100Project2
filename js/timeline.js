ed7a8b7aba39049dfe7c3b7ef2b794679b9fa775
lmc327 <lmc327@cornell.edu>
2019-04-09 22:59:29 -0400

    working on dot plot

--- js/timeline.js
+++ js/timeline.js
@@ -1,29 +1,29 @@
-let svg = d3.select("#artist_dot_plot");
+// let svg = d3.select("#artist_dot_plot");
 
-var margin = {top: 10, right: 30, bottom: 30, left: 30},
-    width = 550 - margin.left - margin.right,
-    height = 480 - margin.top - margin.bottom;
+// var margin = {top: 10, right: 30, bottom: 30, left: 30},
+//     width = 550 - margin.left - margin.right,
+//     height = 480 - margin.top - margin.bottom;
 
-var moma_data;
-var met_data;
-var dateOrderedList = [];
-var display_name = "picasso";
+// var moma_data;
+// var met_data;
+// var dateOrderedList = [];
+// var display_name = "picasso";
 
-// insert method for retreiving name of input person
+// // insert method for retreiving name of input person
 
-d3.json("../datasets/sample_data_moma.json").then( function(moma) {
-    moma.forEach(element => {
-        if (element.name.includes(display_name)){
-            dateOrderedList.push({painting:element.name, year:element.date})
-        }
-        console.log(dateOrderedList);
-    });
-    // var x_scale = d3.scaleLinear()
-    // .range([0, width])
-    // .domain([2, 11]);
+// d3.json("../datasets/sample_data_moma.json").then( function(moma) {
+//     moma.forEach(element => {
+//         if (element.name.includes(display_name)){
+//             dateOrderedList.push({painting:element.name, year:element.date})
+//         }
+//         console.log(dateOrderedList);
+//     });
+//     // var x_scale = d3.scaleLinear()
+//     // .range([0, width])
+//     // .domain([2, 11]);
     
     
     
 
 
-});
+// });
