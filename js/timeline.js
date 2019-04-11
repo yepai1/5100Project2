99d91014f6b0854e202c9ea83c9b61a3202188e4
lmc327 <lmc327@cornell.edu>
2019-04-11 14:02:46 -0400

    met and moma working and redistribution of box heights

--- js/timeline.js
+++ js/timeline.js
@@ -55,9 +55,8 @@ d3.csv(csv).then(function(data) {
         .range([yearMin, yearMax]); 
 
     // draw mini chart @ call update fuction for brushed chart
-    drawChart(x_scale1, miniSvg, 3000, "", .2, metPaintings, "mini");
-    drawChart(x_scale1, miniSvg, 3000, "", .2, momaPaintings, "mini");
-    updateDotChart(1500, 1600);
+    drawChart(x_scale1, miniSvg, 3000, "", .2, data);
+    updateDotChart(1575, 1600);
 
     d3.selectAll(".mini-chart").append("g")
         .attr("class", "brush")
@@ -65,7 +64,7 @@ d3.csv(csv).then(function(data) {
 
     function brushed(){
         start_domain = brushScale(d3.brushSelection(this)[0]);
-        end_domain = start_domain + 100;
+        end_domain = start_domain + 25;
         updateDotChart(start_domain, end_domain);
     };
 
@@ -74,17 +73,15 @@ d3.csv(csv).then(function(data) {
         var x_scale2 = d3.scaleLinear()
             .range([0, width])
             .domain([start, end]); 
-        drawChart(x_scale2, svg, 500, d3.format("d"), 3, momaPaintings, "moma");
-        console.log("here");
-        drawChart(x_scale2, svg, 500, d3.format("d"), 3, metPaintings, "met");
+        // drawChart(x_scale2, svg, 500, d3.format("d"), 3, momaPaintings, "moma");
+        drawChart(x_scale2, svg, 500, d3.format("d"), 4, data);
     }
 
     
     // draw chart based on scale values & svg elt
-
     // function for drawing graphs
     // scale, svg elt, number of bins, format of ticks, circle radius, data set, class
-    function drawChart (x, svg, nbins, format, radius_val, data, class_name){
+    function drawChart (x, svg, nbins, format, radius_val, data){
  // histogram use and adding circles in this section are adapted from: https://bl.ocks.org/gcalmettes/95e3553da26ec90fd0a2890a678f3f69
 
         const histogram = d3.histogram()
@@ -100,7 +97,6 @@ d3.csv(csv).then(function(data) {
         let binContainerEnter = binContainer.enter()
             .append("g")
                 .attr("class", "gBin")
-                .attr("id", class_name)
                 .attr("transform", d => `translate(${x(d.x0)}, ${height})`)
 
         binContainerEnter.selectAll("circle")
@@ -109,15 +105,16 @@ d3.csv(csv).then(function(data) {
                     name: p.Title,
                     artist: p.Artist,
                     value: p.Value,
+                    museum: p.Museum,
                     radius: radius_val
                     }
                 }))
             .enter()
             .append("circle")
-                .attr("class", class_name)
-                .attr("cx", 0) 
-                .attr("cy", function(d) {
-                    return - d.idx * 2 * d.radius - d.radius; })
+                .attr("class", function(d){return d.museum})
+                .attr("cx", function(d){ if (d.idx % 4 != 0){var x_move =d.idx%4; return d.radius * 2 * x_move + 2};}) 
+                .attr("cy", function(d) {var move = d.idx / 4; if (d.idx%4 === 0){var change = d.radius}else if(d.idx%4 === 1){change = d.radius * .5}else if(d.idx%4 === 2){change = 0}else if(d.idx%4 === 3){change = d.radius * -.5}; 
+                    return - move * 2 * d.radius- change;})
                 .attr("r", 0)
                 // .on("mouseover", tooltipOn)
                 // .on("mouseout", tooltipOff)
